48e158049bd7c5cab9a875d91b43cc7c460595f4
Cain Abaddon <ajr.zimmer@gmail.com>
2015-12-30 15:15:21 -0700

    Converting to Line Protocol can differentiate between string and float

--- logToInfluxLineProtocol_Pos.rb
+++ logToInfluxLineProtocol_Pos.rb
@@ -6,18 +6,25 @@ no header into the Line Protocol format that InfluxDB accepts as input.
 This script can be applied to only the position files.
 =end
 
+class String
+  def quote_strings
+     self if Float self rescue "\"" + self + "\""
+  end
+end
+
 logFile = ARGV[0]
 posFields = []
 File.read('posFieldNames.txt').each_line do |line|
     posFields << line.chomp
 end
 logData = []
+dummyTime = 1450733657000 # Sunday Dec 27 ~3PM GMT
 File.readlines(logFile).each do |line|
-    line.gsub!(/\"/, 'NULL')
+    line.gsub!(/\"\"/, 'NULL')
     logData = line.chomp.split(',')
-    logData.map! { |x| "\"" + x + "\""}
-    combined = logData.map.with_index{ |x, i| posFields.at(i) + "=" + x }
-    logInLP = combined.join(",").prepend("LocationReading ")
+    combined = logData.map.with_index{ |x, i| posFields.at(i) + "=" + x.quote_strings }
+    dummyTime += 300000 # Add ~5min
+    logInLP = combined.join(",").prepend("LocationReading ") << " " + dummyTime.floor.to_s
     File.open("FieldsAdded_Pos.txt", "a+") {|file| file.puts(logInLP) }
 end
 
