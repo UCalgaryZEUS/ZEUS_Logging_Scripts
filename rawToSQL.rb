82ed8dd35ee230790fe53ae08fb5ea6aaac3f634
Cain Abaddon <ajr.zimmer@gmail.com>
2016-01-24 14:13:20 -0700

    Adding Drystan's acceleration calculation

--- rawToSQL.rb
+++ rawToSQL.rb
@@ -9,6 +9,9 @@ necessary for use in a MYSQL DB.
 
 GPS_EPOCH = Time.new(1980,1,6,0,0,0,"-07:00")
 UNIX_EPOCH_OFFSET = 315964800 # seconds between Jan 1 1970 and Jan 6 1980
+INITIAL_TIME = 0
+INITIAL_SPEED = 0
+
 
 # used for converting days into seconds
 class Fixnum
@@ -41,6 +44,9 @@ cleanPairData = cleanPairData.flatten.each_slice(2).to_a
 3. Print that extracted information on a single line
 =end
 
+prevTime = INITIAL_TIME
+prevVel = INITIAL_SPEED
+
 cleanPairData.each do |pair|
     singlePair = pair.flatten.join(', ').split(", ")
     # Index 0 should be pos data, 1 should be vel
@@ -52,14 +58,22 @@ cleanPairData.each do |pair|
     #puts humanTime.to_f # seconds since Unix Epoch
     #puts humanTime.to_f - 315964800 # seconds since GPS epoch 
 
+    timeElapsed = gpsSec - prevTime
+    prevTime = gpsSec
+
     vertSpd = velChunk.at(13).to_f
     direction = velChunk.at(14).to_f #in degrees with respect to True North
     horiSpd = velChunk.at(15).to_f
-    #velocity = "[" + vertSpd.to_s + " " + horiSpd.to_s + " " + direction.to_s + "]"
     velocity = Math.sqrt(vertSpd**2 + horiSpd**2) # Probably not accurate at all
+
+    acceleration = (velocity - prevVel)/(timeElapsed)
+    prevVel = velocity
+    velocity = velocity.round(5)
+    acceleration = acceleration.round(5)
+    
     lat = posChunk.at(11).to_f
     long = posChunk.at(12).to_f
     altitude = posChunk.at(13).to_f
-    dataForSQLDB = humanTime.strftime("%Y-%m-%d %H:%M:%S.%L") + " [acceleration calculation here] " + velocity.to_s + " " + lat.to_s + " " + long.to_s + " " + altitude.to_s
+    dataForSQLDB = humanTime.strftime("%Y-%m-%d %H:%M:%S.%L") + "," + acceleration.to_s + "," + velocity.to_s + "," + lat.to_s + "," + long.to_s + "," + altitude.to_s
     File.open("cleanDataForSQLDB.txt", "a+") {|file| file.puts(dataForSQLDB) }
 end
