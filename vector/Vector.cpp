47ff2093aea284c9239d8ddcf10db20dff0bf4a7
gloopy <drystan.mazur@gmail.com>
2016-03-18 20:13:58 -0600

    Updates with a final defition what lamba and phi where in this context

--- vector/Vector.cpp
+++ vector/Vector.cpp
@@ -1,9 +1,14 @@
 #include <boost/algorithm/string/split.hpp>
 #include <boost/algorithm/string.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/io.hpp>
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
+
+#define REFLAT    51.080609
+#define REFLONG -114.130981
 
 using namespace boost::algorithm;
 
@@ -15,6 +20,13 @@ int main (int argc,char** argv){
 std::ifstream inFile ("wed242016.gps");
 std::string dataline;
 std::vector<std::string> data;
+matrix<double> transForm (3, 3); //Creates a 3x3 matrix
+
+
+float x_vect;
+float y_vect;
+float z_vect;
+
 
 
 	if(inFile.is_open()){
